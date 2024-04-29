6d53e8f15971bb1ab7a10b18b499d238131528fb
Vespucci95 <donglonk@naver.com>
2024-04-30 00:11:06 +0900

    style: transition 속성 변경

--- 02_올림픽_통계_서비스_최적화/lecture-2-master/src/components/Bar.js
+++ 02_올림픽_통계_서비스_최적화/lecture-2-master/src/components/Bar.js
@@ -46,7 +46,7 @@ const BarGraph = styled.div`
     width: 100%;
     transform: scaleX(${({width}) => width / 100});
     transform-origin: center left;
-    transition: width 1.5s ease;
+    transition: transform 1.5s ease;
     height: 100%;
     background: ${({isSelected}) => isSelected ? 'rgba(126, 198, 81, 0.7)' : 'rgb(198, 198, 198)'};
     z-index: 1;
